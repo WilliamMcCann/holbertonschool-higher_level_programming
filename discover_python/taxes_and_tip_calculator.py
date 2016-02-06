5132e35022468e1a8b3985972cf8c4fb8d422d4a
William McCann <45@holbertonschool.com>
2016-02-06 14:31:20 -0800

    corrected spacing

--- discover_python/taxes_and_tip_calculator.py
+++ discover_python/taxes_and_tip_calculator.py
@@ -11,14 +11,14 @@
 print "Welcome to the taxes and tip calculator!"
 
 """gets input on the meal price and assigns it to variable "meal".  Converts that to an int so it can be used for math"""
-meal = float(raw_input("What is the price before tax?"))
+meal = float(raw_input("What is the price before tax? "))
 
 """gets input on the tax amount and assigns it to variable "taxes" Converts that to an int so it can be used for math"""
-taxes = float(raw_input("What are the taxes? (in %)"))
+taxes = float(raw_input("What are the taxes? (in %) "))
 
 """gets input on the tip amount and assigns it to variable "tip" Converts th\
 at to an int so it can be used for math"""
-tip = float(raw_input("What do you want to tip? (in %)"))
+tip = float(raw_input("What do you want to tip? (in %) "))
 
 """divides variable "taxes" by 100 to convert it to a percentage.  Multiplies that by "meal" to get the amount of the tip.  Adds that to meal to get the amount of the meal and taxes combined.  Assigns that to variable "meal_and_taxes" """
 meal_and_taxes = meal + (meal * (taxes / 100))
