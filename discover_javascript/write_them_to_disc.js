b46186efffb0c7fb589a176c6164fdc04c4511c2
William McCann <45@holbertonschool.com>
2016-02-15 10:23:24 -0800

    uploading mandatory javascript files

---
+++ discover_javascript/write_them_to_disc.js
@@ -0,0 +1,37 @@
+var https = require('https');
+var fs = require('fs')
+
+var options = {
+    hostname: 'api.github.com',
+    path: '/search/repositories?q=language:javascript&sort=stars\
+&order=desc',
+    headers: {
+	'User-Agent': 'Holberton_School',
+	'Authorization': 'token ' + process.env.TOKEN
+    }
+}
+
+var request = https.request(options, function(res) {
+    streamToString (res, (jsonString) => {
+	console.log("The file was saved!");
+	fs.writeFile('/tmp/45', jsonString, function (err) {
+	    if (err) return console.log(err);
+	});
+    });
+});
+request.end();
+
+request.on('error', function(e) {
+    console.error(e);
+});
+
+function streamToString(stream, cb) {
+    const chunks = [];
+    stream.on('data', (chunk) => {
+	chunks.push(chunk);
+    });
+    stream.on('end', () => {
+	cb(chunks.join(''));
+    });
+}
+
