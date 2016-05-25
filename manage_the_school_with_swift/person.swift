204d666b505e731b430c717abbb78e2ca85743c5
Nathan McCann <45@p2-3.hbtn.io>
2016-05-24 17:22:39 -0700

    completed Task 1

--- manage_the_school_with_swift/person.swift
+++ manage_the_school_with_swift/person.swift
@@ -1,16 +1,35 @@
 class Person {
-    
+
+    //Public attributes
     var first_name : String
     var last_name : String
     var age : Int
 
+    //Public methods
     init(first_name: String, last_name: String, age: Int) {
         self.first_name = first_name
-        self.last_name = first_name
+        self.last_name = last_name
         self.age = age
     }
 
     func fullName() -> String{
         return "\(first_name) \(last_name)"
+    }
+}
+
+//Protocol
+protocol Classify{
+    func isStudent() -> Bool
+}
+
+class Mentor: Person, Classify {
+    func isStudent() -> Bool {
+    return false
+    }
+}
+
+class Student: Person, Classify {
+    func isStudent() -> Bool {
+    return true
     }
 }
