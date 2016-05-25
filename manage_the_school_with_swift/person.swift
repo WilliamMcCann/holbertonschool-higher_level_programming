0f25bf08fee2a3d1cf5d95d7465524b692b7254d
Nathan McCann <45@p2-3.hbtn.io>
2016-05-24 20:52:01 -0700

    adding two

--- manage_the_school_with_swift/person.swift
+++ manage_the_school_with_swift/person.swift
@@ -22,9 +22,38 @@ protocol Classify{
     func isStudent() -> Bool
 }
 
+//Enumeration
+enum Subject {
+    case Math
+    case English
+    case French
+    case History
+}
+
 class Mentor: Person, Classify {
+    init(first_name: String, last_name: String, age: Int, subject: Subject = Subject.Math) {
+        self.subject = subject
+        super.init(first_name: first_name, last_name: last_name, age: age)
+    }
+
+
     func isStudent() -> Bool {
     return false
+    }
+
+    let subject: Subject
+
+    func stringSubject() -> String {
+        switch self.subject {
+            case Subject.Math:
+                return "Math"
+            case Subject.English:
+                return "English"
+            case Subject.French:
+                return "French"
+            case Subject.History:
+                return "History"
+        }
     }
 }
 
