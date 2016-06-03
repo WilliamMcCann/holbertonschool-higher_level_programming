73ae0137a2f69356cb400e83a53b32a75f238498
Nathan McCann <45@p2-3.hbtn.io>
2016-06-02 21:51:27 -0700

    completed through two

---
+++ basic_sql_playground/1-main.sql
@@ -0,0 +1,10 @@
+/*script to return first_name, age, and color of Person and EyesColor*/
+SELECT first_name
+FROM Person;
+SELECT first_name, age
+FROM Person;
+SELECT DISTINCT color
+FROM EyesColor;
+SELECT first_name, last_name, age
+FROM Person
+ORDER BY age ASC;
---
+++ basic_sql_playground/2-main.sql
@@ -0,0 +1,8 @@
+/*add and update values in a database*/
+INSERT INTO Person (first_name,last_name,age)
+VALUES ('Jon','Snow',26);
+INSERT INTO Person (first_name,last_name,age)
+VALUES ('Arya','Stark',12);
+SELECT *
+FROM Person
+ORDER BY last_name ASC;
