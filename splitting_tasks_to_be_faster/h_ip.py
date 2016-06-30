8c4c487f2c8433aadc171af010c74de42c8660e3
William McCann <nwilliammccann@gmail.com>
2016-06-29 19:35:00 -0700

    adding one

--- splitting_tasks_to_be_faster/h_ip.py
+++ splitting_tasks_to_be_faster/h_ip.py
@@ -1,12 +1,17 @@
 import threading
 import requests
+import json
 
 class IPThread(threading.Thread):
     def __init__(self, ip, callback):
+        threading.Thread.__init__(self)
 
         self.__ip = ip
         self.__callback = callback
 
     def run(self):
-        r = requests.get('https://api.ip2country.info/' + str(self.__ip))
-        self.__callback("r")
+        r = requests.get('https://api.ip2country.info/ip?' + str(self.__ip))
+        obj = json.loads(r.text)
+        self.__callback(obj.get('countryName').encode('utf-8'))
+        print "Search: " + str(self.__ip)
+        print "countryName: " + obj.get('countryName').encode('utf-8')
